a4d30ae4f70eb79e96c678c804d6203cc5d0c39f
jakey22 <sunjeon7@gmail.com>
2020-10-20 14:00:06 +0900

    mongo update

--- gather_data/get_weather.py
+++ gather_data/get_weather.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 ## openWeather - get weather info
 ############################################
 def weather():
-    with MongoClient('mongodb://192.168.219.104:27017') as client:
+    with MongoClient('mongodb://192.168.0.136:27017') as client:
         db = client.mydb
         data_list = list(db.mountain.find({}))
 
@@ -13,15 +13,9 @@ def weather():
             API_key = 'd9a33dfaa19a045b98d9e6f57ce733ba'
             url = f'https://api.openweathermap.org/data/2.5/onecall?lat={data["mt_lat"]}&lon={data["mt_lon"]}&units=metric&appid={API_key}'
             res = requests.get(url).json()
-            # print(res['current']['dt'])
-            # print(res['current']['sunrise'])
-            # print(res['current']['sunset'])
-            # print(res['current']['temp'])
-            # print(res['current']['pressure'])
-            # print(res['current']['humidity'])
-            # print(res['current']['wind_speed'])
-            # print(res['current']['wind_deg'])
-            ata_temp['mt_name']=data['mt_name']
+
+            data_temp['mt_name']=data['mt_name']
+            data_temp['mt_num']=data['mt_num']
             data_temp['mt_weather_main']=res['current']['weather'][0]['main']
             data_temp['temp']= res['current']['temp']
             data_temp['sunrise']=res['current']['sunrise']
@@ -29,8 +23,8 @@ def weather():
             data_temp['wind_speed']=res['current']['wind_speed']
             data_temp['wind_deg']=res['current']['wind_deg']
 
-            # print(res['current']['weather'][0]['description'])
             #db.weather.insert(data_temp)
-            db.weather.update({'mt_name':data_temp['mt_name']},{ '$set':{'mt_weather_main': data_temp['mt_weather_main']}})
+            db.weather.update({'mt_name':data_temp['mt_name']},{ '$set':{'mt_num':data_temp['mt_num'],'mt_weather_main': data_temp['mt_weather_main'],'temp': data_temp['temp'],'sunrise':data_temp['sunrise'],'sunset':data_temp['sunset'],'wind_speed':data_temp['wind_speed'],'wind_deg':data_temp['wind_deg']}})
+            # { '$set':{data_temp}}})
 weather()
         
