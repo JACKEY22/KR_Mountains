b73ed7a3c5197b09d3834e483b8596e4bc4e3199
jackey22 <allensiri@naver.com>
2020-10-21 04:43:17 +0900

    #

--- mt/views.py
+++ mt/views.py
@@ -15,10 +15,11 @@ def krmt(request):
     center = [37.541, 126.986]
     m = folium.Map(center, zoom_start=7)
 
-    with MongoClient('mongodb://192.168.0.136') as client:
+    with MongoClient('mongodb://192.168.219.104:27017') as client:
         db = client.mydb 
         mt_data_list = list(db.mountain.find({}))
         wt_data_list = list(db.weather.find({}))
+
         for mt_data, wt_data in zip(mt_data_list,wt_data_list):
             lat_lon = [mt_data['mt_lat'], mt_data['mt_lon']]
             pop_text = folium.Html(
@@ -38,14 +39,30 @@ def krmt(request):
     return render(request, 'mt/krmt.html', context={'page_data':page_data, 'map':m})
     
 def krmt_detail(request,mt_num):
-    with MongoClient('mongodb://192.168.0.136:27017') as client:
+    with MongoClient('mongodb://192.168.219.104:27017') as client:
         db = client.mydb 
 
         mt_data = db.mountain.find({"mt_num" : mt_num})[0]
         wt_data = db.weather.find({"mt_num" : mt_num})[0]
+        acc_data = db.acc.find({"mt_num" : mt_num})[0]
+        hp_data = db.hp.find({"mt_num" : mt_num})[0]
+        pm_data = db.pm.find({"mt_num" : mt_num})[0] #lat lon
 
         lat_lon = [mt_data['mt_lat'], mt_data['mt_lon']]
         m = folium.Map(lat_lon, zoom_start=12)
+        if acc_data['acc_name'] != "None":
+            acc_lat_lon = [mt_data['mt_lat'], mt_data['mt_lon']]
+            folium.Marker(acc_lat_lon, tooltip=acc_data['acc_name'], icon=folium.Icon(color='gray',icon='home')).add_to(m)
+
+        if hp_data['hp_name'] != "None": 
+            hp_lat_lon = [hp_data['hp_lat'], hp_data['hp_lon']]
+            folium.Marker(hp_lat_lon, tooltip=hp_data['hp_name'] ,icon=folium.Icon(color='red',icon='star')).add_to(m)
+
+        if pm_data['pm_name'] != "None":   
+            pm_lat_lon = [pm_data['pm_lat'], pm_data['pm_lon']]
+            folium.Marker(pm_lat_lon, tooltip=pm_data['pm_name'] ,icon=folium.Icon(color='green')).add_to(m)
+
+        
 
         pop_text = folium.Html(
                 f"<img src={mt_data['mt_img_path_preview']} width=300px; height=300px;><br>" +
@@ -55,6 +72,9 @@ def krmt_detail(request,mt_num):
             )
         pop_up = folium.Popup(pop_text)
         folium.Marker(lat_lon, popup=pop_up, tooltip=mt_data['mt_name']).add_to(m)
+         # color
+        
+        
         
         m = m._repr_html_()
     return render(request, 'mt/krmt_detail.html',context={'map':m})
@@ -64,7 +84,7 @@ def acc(request):
     center = [37.541, 126.986]
     m = folium.Map(center, zoom_start=7)
 
-    with MongoClient('mongodb://192.168.0.136:27017') as client:
+    with MongoClient('mongodb://192.168.219.104:27017') as client:
         db = client.mydb 
         data_list = list(db.mountain.find({}))
         data_list2 = list(db.weather.find({}))
