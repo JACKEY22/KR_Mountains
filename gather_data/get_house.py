1d172a389c2984f1016bd9fea286a99a7fe4b94d
jakey22 <sunjeon7@gmail.com>
2020-10-20 17:50:26 +0900

    #

--- gather_data/get_house.py
+++ gather_data/get_house.py
@@ -6,11 +6,11 @@ res = requests.get(url=url)
 data = json.loads(res.content)
 jdata = data[1]
 #print(jdata.keys())
-#print(jdata['dsList'])
+print(jdata['dsList'])
 
-print(jdata['dsList'][0]['UPP_AIS_TP_NM']
-,jdata['dsList'][0]['AIS_TP_CD_NM']
-,jdata['dsList'][0]['PAN_NM']
-,jdata['dsList'][0]['CNP_CD_NM']
-,jdata['dsList'][0]['PAN_SS']
-,jdata['dsList'][0]['DTL_URL'])
+print(jdata['dsList'][0]['UPP_AIS_TP_NM'])
+# ,jdata['dsList'][0]['AIS_TP_CD_NM']
+# ,jdata['dsList'][0]['PAN_NM']
+# ,jdata['dsList'][0]['CNP_CD_NM']
+# ,jdata['dsList'][0]['PAN_SS']
+# ,jdata['dsList'][0]['DTL_URL'])
